c8c4103b643ee6b270250da60bcb53ed556b3025
Francesco Ghizzo <Francesco@Francescos-MacBook-Pro.local>
2020-07-13 09:55:10 +0200

    Corretto calcolo della portata nella funzione MotoUniforme (non più ricorsivo)

--- Esercitazione 1 - Dati e codici/Engelund_metodo_trapezi.py
+++ Esercitazione 1 - Dati e codici/Engelund_metodo_trapezi.py
@@ -123,7 +123,6 @@ def MotoUniforme( iF, y, z, ks, Y, NG=2 ):
     xj, wj = GaussPoints( NG ) # Calcola i punti e i pesi di Gauss
 
     # Inizializzo
-    Q = 0 # Portata  
     Omega = 0 # Area bagnata
     b = 0 # Larghezza superficie libera
     num_alpha = 0 # Numeratore di alpha
@@ -184,7 +183,8 @@ def MotoUniforme( iF, y, z, ks, Y, NG=2 ):
             num_alpha = num_alpha + (cos_phi**2)*((ks[i]**3)*(Yi[i]**3) + ((ks[i+1]**3)*(Yi[i+1]**3)))*0.5*dy
             num_beta = num_beta + (cos_phi**(4/3))*((ks[i]**2)*(Yi[i]**(7/3)) + (ks[i+1]**2)*(Yi[i+1]**(7/3)))*0.5*dy
             den = den + (cos_phi**(2/3))*(ks[i]*Yi[i]**(5/3)+ks[i+1]*Yi[i+1]**(5/3))*0.5*dy
-            Q = Q + den*np.sqrt(iF)
+
+    Q = den*np.sqrt(iF)
 
     if den != 0: # Evito la divisione per 0  
         alpha = (Omega**2)*num_alpha/(den**3)
