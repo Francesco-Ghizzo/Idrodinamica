453e08b685f0f2b598867429f74bd438daf1a6b2
Francesco Ghizzo <Francesco@Francescos-MacBook-Pro.local>
2020-07-15 08:56:54 +0200

    Nella funzione MotoUniforme, num_alpha, num_beta e den calcolati con i nodi Yj ed i pesi wj di Gauss invece che con il metodo dei trapezi

--- Esercitazione 1 - Dati e codici/Engelund_quadratura_Gauss.py
+++ Esercitazione 1 - Dati e codici/Engelund_quadratura_Gauss.py
@@ -81,8 +81,7 @@ def GaussPoints(NG):
                        (18+np.sqrt(30))/36,
                        (18-np.sqrt(30))/36
         ])
-        
-    # espandere il numero di punti di gauss
+
     return p, w
 
 
@@ -160,9 +159,12 @@ def MotoUniforme( iF, y, z, ks, Y, NG=2 ):
     
             b = b + dy
             Omega = Omega + (Yi[i]+Yi[i+1])*0.5*dy
-            num_alpha = num_alpha + (cos_phi**2)*((ks[i]**3)*(Yi[i]**3) + ((ks[i+1]**3)*(Yi[i+1]**3)))*0.5*dy
-            num_beta = num_beta + (cos_phi**(4/3))*((ks[i]**2)*(Yi[i]**(7/3)) + (ks[i+1]**2)*(Yi[i+1]**(7/3)))*0.5*dy
-            den = den + (cos_phi**(2/3))*(ks[i]*Yi[i]**(5/3)+ks[i+1]*Yi[i+1]**(5/3))*0.5*dy
+            ksj = (ks[i+1]-ks[i])*0.5*np.array(xj) + (ks[i]+ks[i+1])*0.5
+            Yj = (Yi[i+1]-Yi[i])*0.5*np.array(xj) + (Yi[i]+Yi[i+1])*0.5
+            num_alpha = num_alpha + dy*0.5*(cos_phi**2)*np.sum(wj*(ksj**3)*(Yj**3))
+            num_beta = num_beta + dy*0.5*(cos_phi**(4/3))*np.sum(wj*(ksj**2)*(Yj**(7/3)))
+            den = den + dy*0.5*(cos_phi**(2/3))*np.sum(wj*ksj*Yj**(5/3))
+
 
     Q = den*np.sqrt(iF)
 
