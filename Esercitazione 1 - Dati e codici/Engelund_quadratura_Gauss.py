8012ad960ef5ca13b829bd8028bbfab405542f7b
Francesco Ghizzo <Francesco@Francescos-MacBook-Pro.local>
2020-07-15 10:55:57 +0200

    funzione xrange() sostituita con range()

--- Esercitazione 1 - Dati e codici/Engelund_quadratura_Gauss.py
+++ Esercitazione 1 - Dati e codici/Engelund_quadratura_Gauss.py
@@ -132,7 +132,7 @@ def MotoUniforme( iF, y, z, ks, Y, NG=2 ):
     N = Yi.size # Numero di punti sulla trasversale
 
     # N punti trasversali -> N-1 intervalli (trapezi)
-    for i in xrange( N-1 ): # Per ogni trapezio
+    for i in range( N-1 ): # Per ogni trapezio
 
         if not (Yi[i] <= 0 and Yi[i+1] <= 0): # Non considero i tratti con tirante idraulico negativo (al di sopra della superficie dell'acqua)
         
@@ -212,7 +212,7 @@ def Critica( iF, y, z, ks, Q, MAXITER=100, tol=1e-03, NG=2 ):
     # Calcolo della profondita' critica con il metodo dicotomico
     # La funzione da annullare e' quella per l'eccesso di carico specifico sulla sezione
 
-    for i in xrange(MAXITER):
+    for i in range(MAXITER):
         Ym = (Ya+Yb)/2
         fYm = Energia( iF, y, z, ks, Ym, Q, NG)
         fYa = Energia( iF, y, z, ks, Ya, Q, NG)
@@ -282,7 +282,7 @@ Yc = np.zeros( vpoints ) # Tirante critico
 # -----------------
 # Per ciascun livello della superficie libera calcolo la portata defluente in condizioni
 # di moto uniforme, i coefficienti di ragguaglio e la geometria della sezione bagnata 
-for n in xrange( vpoints ):
+for n in range( vpoints ):
     
     # Calcola i parametri di moto uniforme assegnato il tirante
     Q[n], Omega[n], b[n], alpha[n], beta[n] = MotoUniforme( iF, y, z, ks, Y[n], NG=NG )
